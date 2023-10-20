d753ee1ebc44c06f43b50881a39156e4aa31ec51
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-20 18:21:33 +0200

    edit a second object

--- database/db.php
+++ database/db.php
@@ -6,12 +6,24 @@ require __DIR__ . '/../models/Kennel.php';
 require __DIR__ . '/../models/Toy.php';
 require __DIR__ . '/../models/Food.php';
 
+$ecommerce = [];
 
-
+// Palla Cane
+$pallaCategory = new Category('dog');
 $pallaMSize = new Toy('m');
 $pallaMaterial = new Kennel('plastic');
-$pallaCategory = new Category('dog');
 
 $palla = new Product($pallaCategory, 'palla da tennis', 3.99, 150, '10cm', '🐶', 'https://picsum.photos/200/300', $pallaMaterial, $pallaMSize);
+
+// Biscotti Gatto
+$biscuitsCategory = new Category('cat');
+$biscuitsOne = new Food('1kg', 'Medium', 'Puppy');
+
+$biscuits = new Product($biscuitsCategory, 'BiscuitsOne', 15.49, 20, '25x25', '🐱', 'https://picsum.photos/200/300', $biscuitsOne);
+
+
+/* array_push($ecommerce, $palla); */
+
+
 
 var_dump($palla);
--- models/Product.php
+++ models/Product.php
@@ -2,20 +2,20 @@
 class Product extends Category
 {
 
-    public $name;
     public $category;
+    public $name;
     public $price;
-    public $quantity;
+    public $stock_quantity;
     public $dimension;
     public $icon;
     public $img_path;
 
-    public function __construct(Category $category, $name, $price, $quantity, $dimension, $icon, $img_path)
+    public function __construct(Category $category, $name, $price, $stock_quantity, $dimension, $icon, $img_path)
     {
         $this->category = $category;
         $this->name = $name;
         $this->price = $price;
-        $this->quantity = $quantity;
+        $this->stock_quantity = $stock_quantity;
         $this->dimension = $dimension;
         $this->icon = $icon;
         $this->img_path = $img_path;
