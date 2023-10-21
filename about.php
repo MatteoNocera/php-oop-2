a7150088cb9ef4b3ff37abf518a05e67787a9dfa
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-21 20:02:57 +0200

    fix footer

--- about.php
+++ about.php
@@ -10,8 +10,13 @@ include __DIR__ . '/layouts/head.php';
 <main>
 
     <?php include __DIR__ . '/layouts/partials/jumbo_about.php' ?>
-    <h2>Faq</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ex inventore labore, magnam natus autem impedit omnis distinctio quasi numquam itaque animi necessitatibus blanditiis aliquam est assumenda nam voluptatum odio nisi alias perferendis aut architecto quam! Ex omnis quidem quos mollitia in. Aliquam soluta sequi doloribus et fugiat quod, hic placeat ab corporis, porro dolore. Sequi sed soluta molestiae dolore modi possimus exercitationem laborum cum illum quod consequatur reprehenderit, porro eaque, excepturi fuga aut, totam ipsam! Perferendis eligendi deserunt magnam recusandae nobis facere minus, magni laborum, quibusdam, accusantium dicta hic est saepe error ad perspiciatis nostrum eos! Dignissimos, labore dolorem. Alias cupiditate quae voluptate possimus quidem magni dignissimos illo nisi pariatur temporibus, cumque, distinctio blanditiis. Molestias laudantium temporibus repudiandae impedit in consequatur, molestiae iste omnis itaque quidem ea nesciunt ad rem sapiente magni! Est reiciendis ipsa eaque repellat temporibus sit totam, accusamus ducimus possimus maxime eos minima incidunt laborum ut.</p>
+    <div class="container">
+        <h2>Faq</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ex inventore labore, magnam natus autem impedit omnis distinctio quasi numquam itaque animi necessitatibus blanditiis aliquam est assumenda nam voluptatum odio nisi alias perferendis aut architecto quam! Ex omnis quidem quos mollitia in. Aliquam soluta sequi doloribus et fugiat quod, hic placeat ab corporis, porro dolore. Sequi sed soluta molestiae dolore modi possimus exercitationem laborum cum illum quod consequatur reprehenderit, porro eaque, excepturi fuga aut, totam ipsam! Perferendis eligendi deserunt magnam recusandae nobis facere minus, magni laborum, quibusdam, accusantium dicta hic est saepe error ad perspiciatis nostrum eos! Dignissimos, labore dolorem. Alias cupiditate quae voluptate possimus quidem magni dignissimos illo nisi pariatur temporibus, cumque, distinctio blanditiis. Molestias laudantium temporibus repudiandae impedit in consequatur, molestiae iste omnis itaque quidem ea nesciunt ad rem sapiente magni! Est reiciendis ipsa eaque repellat temporibus sit totam, accusamus ducimus possimus maxime eos minima incidunt laborum ut.
+
+        </p>
+    </div>
 
 </main>
 
